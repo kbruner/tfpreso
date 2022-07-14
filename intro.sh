e0de98895383c227a55f0d5c292ba4cb53dfc3aa
K <kbruner@users.noreply.github.com>
2022-07-13 19:18:02 -0700

    more visual tweaks

--- intro.sh
+++ intro.sh
@@ -4,12 +4,19 @@ width=80
 
 clear
 figlet -c -w "$width" -f standard 'From Zero'
+echo
+echo
+echo
 figlet -c -w "$width" -f mini 'to'
+echo
+echo
+echo
 figlet -c -w "$width" -f slant 'Hello Terraform'
 
 echo
 echo
-sleep 5
+echo
+echo
 
 cd terraform
 
@@ -22,8 +29,18 @@ while [ true ]; do
   fi
 done
 
+clear
+
+figlet -f standard "terraform init"
+
+echo
+echo
+echo
+
+echo '$ terraform init'
 time terraform init
 
+echo
 echo
 echo
 echo 'Hit enter to kick off terraform apply'
@@ -35,4 +52,15 @@ while [ true ]; do
   fi
 done
 
+clear
+
+figlet -f standard "terraform apply"
+
+echo
+echo
+echo
+echo '$ terraform apply -auto-approve'
 time terraform apply -auto-approve
+echo
+echo
+echo
